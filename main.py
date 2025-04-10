0e1712e297d714e34876e4e82c2125a5cd9b6475
rajendravaddi <149449081+rajendravaddi@users.noreply.github.com>
2025-04-10 14:21:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-COLAB_URL = "https://cba9-34-80-19-133.ngrok-free.app/model-predict"  # Replace with actual ngrok URL
+COLAB_URL = "https://be53-34-44-215-91.ngrok-free.app/model-predict"  # Replace with actual ngrok URL
+
 
 @app.post("/predict")
 async def forward_to_colab(file: UploadFile = File(...)):
     files = {'file': (file.filename, await file.read(), file.content_type)}
     response = requests.post(COLAB_URL, files=files)
     return JSONResponse(content=response.json())
-
